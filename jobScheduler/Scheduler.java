143b816641acc5bbdca8908030b0b522976de71f
sophie <2amslekshun@gmail.com>
2019-04-05 23:06:23 -0700

    Add todo for alternate approach

--- jobScheduler/Scheduler.java
+++ jobScheduler/Scheduler.java
@@ -2,6 +2,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 class Scheduler {
+  // TODO look into implementing this as ScheduledThreadPoolExecutor
   private static Timer timer = new Timer("mdm+");
 
   public static void schedule(Runnable function, int delay) {
