90e174fe9ffbb7e609f1a77259ef9244d5675c79
sophie <2amslekshun@gmail.com>
2019-04-05 23:14:41 -0700

    Remove argument printing

--- jobScheduler/Main.java
+++ jobScheduler/Main.java
@@ -1,9 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    for (String arg : args) {
-      System.out.println(arg);
-    }
-
     Scheduler.schedule(
       () -> {
         System.out.println("Your random value is: "
