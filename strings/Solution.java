dda13245478c9088124c7aa1f290b0dc1539ffdb
sophie <2amslekshun@gmail.com>
2019-04-29 17:07:10 -0700

    Find longest palindrome, incomplete

--- strings/Solution.java
+++ strings/Solution.java
@@ -23,4 +23,41 @@ class Solution {
     }
     return maxCount;
   }
+
+  // Initial solution, does not cover definition of palindrome
+  // including zero string dividers.
+  // Ex. abba is not a palindrome 
+  // Ex. aabaa is a palindrome
+  // Ex. b is a palindrome
+  // Ex. aaccaa is not a palindrome
+  public String longestPalindrome(String a) {
+    int pivot = a.length() / 2;
+    int lo = 0;
+    int hi = (pivot * 2) + 1;
+    
+    StringBuilder s = new StringBuilder(a);
+    
+    while (lo < s.length()) {
+      if (hi > s.length()) {
+        pivot -= 1;
+        lo = 0;
+        hi = (pivot * 2) + 1;
+      } else {
+        String one = s.substring(lo, lo + pivot + 1);
+        String two = s.substring(lo + pivot, hi);
+        StringBuilder temp = new StringBuilder(two);
+        String revtwo = temp.reverse().toString();
+        if (one.equals(revtwo)) {
+          String result = one + two.substring(1);
+          return result;
+        } else {
+          lo += 1;
+          hi += 1;
+        }
+      }
+    }
+    
+    return "";
+    // Or, throw IllegalArgumentException
+  }
 }
