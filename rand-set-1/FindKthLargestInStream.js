d11fd7a6d53097331b2bc3325972195ed9af5ae3
sophie <2amslekshun@gmail.com>
2019-06-13 17:02:10 -0700

    Find kth largest in stream
    Not too efficient. For one, we only need need to keep track of the 1-kth largest elements.
    We can throw out values less than the kth.

---
+++ rand-set-1/FindKthLargestInStream.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function (k, nums) {
+  this.kthLarge = k
+  this.listOfStream = nums.sort((a, b) => a - b)
+};
+
+/** 
+* @param {number} val
+* @return {number}
+*/
+KthLargest.prototype.add = function (val) {
+  // TODO: This should be a binary search, pretty sure findIndex is O(n) 
+  let indexToInsertInto = this.listOfStream.findIndex((el) =>
+    el > val
+  )
+
+  if (indexToInsertInto == -1) {
+    indexToInsertInto = this.listOfStream.length
+  }
+
+  this.listOfStream.splice(indexToInsertInto, 0, val)
+
+  return this.listOfStream[this.listOfStream.length - k]
+};
+
+
+let k = 3;
+let arr = [4, 5, 8, 2];
+let kthLargest = new KthLargest(3, arr);
+console.log(kthLargest.add(3))   // returns 4
+console.log(kthLargest.add(5))  // returns 5
+console.log(kthLargest.add(10))  // returns 5
+console.log(kthLargest.add(9))   // returns 8
+console.log(kthLargest.add(4))   // returns 8
