35223292b79b0fe3f3749899bd966ec6c8b35c06
sophie <2amslekshun@gmail.com>
2019-12-08 01:46:17 -0800

    Add question a peer was thinking about

---
+++ numbers/LongestSubArrayLessThanK.js
@@ -0,0 +1,41 @@
+const assert = require('assert')
+
+/**
+ * Takes an array and int, finds longest subarray summing to <= k
+ * arr contains integers from -infinity to +infinity
+ * 
+ * O(n^2)
+ */
+let longestSubarray = (arr, k) => {
+  let curLength = arr.length
+  let curSum
+
+  for (let i = 0; i < curLength; curLength -= 1) {
+    for (let j = 0; j + curLength < arr.length; j += 1) {
+      let curArr = arr.slice(j, j + curLength)
+      curArr.forEach((el) => {
+        curSum += el
+      })
+
+      if (curSum < k) {
+        return curArr
+      }
+
+      curSum = 0
+    }
+  }
+  
+  // No value sums to less than k
+  return []
+}
+
+let result = longestSubarray([9,-2,4,8], 5) 
+console.table(result)
+assert.deepEqual(result, [-2, 4])
+
+result = longestSubarray([-19, 8, 0, 100, 70, -43], 30)
+assert.deepEqual(result, [-19, 8, 0])
+
+module.export = {
+  longestSubarray
+}
