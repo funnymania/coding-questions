e24ac25e809a83cedcf1375139887b8dd9bd9f61
sophie <2amslekshun@gmail.com>
2019-12-09 03:22:13 -0800

    Use prefix sums to reduce extra work

--- numbers/LongestSubArrayLessThanK.js
+++ numbers/LongestSubArrayLessThanK.js
@@ -1,24 +1,30 @@
 const assert = require('assert')
 
 /**
- * Takes an array and int, finds longest subarray summing to <= k
- * arr contains integers from -infinity to +infinity
+ * Takes an array of int and an int, finds longest subarray summing to <= k.
+ * arr contains integers from -infinity to +infinity. k is a positive integer.
  * 
  * O(n^2)
  */
 let longestSubarray = (arr, k) => {
   let curLength = arr.length
-  let curSum
+  let curSum = 0
+  let prefixSum = []
 
+  // Generate prefix sums
+  arr.forEach(el => {
+    prefixSum.push(curSum)
+    curSum += el
+  })
+  prefixSum.push(curSum)
+
+  curSum = 0
   for (let i = 0; i < curLength; curLength -= 1) {
-    for (let j = 0; j + curLength < arr.length; j += 1) {
-      let curArr = arr.slice(j, j + curLength)
-      curArr.forEach((el) => {
-        curSum += el
-      })
+    for (let j = 0; j + curLength <= arr.length; j += 1) {
+      let curSum = prefixSum[j + curLength] - prefixSum[j]
 
       if (curSum < k) {
-        return curArr
+        return arr.slice(j, j + curLength)
       }
 
       curSum = 0
@@ -30,11 +36,14 @@ let longestSubarray = (arr, k) => {
 }
 
 let result = longestSubarray([9,-2,4,8], 5) 
-console.table(result)
 assert.deepEqual(result, [-2, 4])
 
-result = longestSubarray([-19, 8, 0, 100, 70, -43], 30)
-assert.deepEqual(result, [-19, 8, 0])
+result = longestSubarray([-19, -800, 8000000, -100, -70, -4300], 30)
+assert.deepEqual(result, [-100, -70, -4300])
+
+result = longestSubarray([0, 0, -80, 82, 0, -4300], 1)
+assert.deepEqual(result, [0, 0, -80, 82, 0, -4300])
+console.table(result)
 
 module.export = {
   longestSubarray
