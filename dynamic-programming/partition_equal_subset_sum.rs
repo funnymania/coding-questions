2aa468b66c329386b8f098a47fe25156d1937bd3
Michael D. McClure <36145527+funnymania@users.noreply.github.com>
2021-05-24 13:10:30 -0700

    Add brute force method

--- dynamic-programming/partition_equal_subset_sum.rs
+++ dynamic-programming/partition_equal_subset_sum.rs
@@ -1,3 +1,5 @@
+// LeetCode #416: https://leetcode.com/problems/partition-equal-subset-sum/
+
 use std::collections::HashSet;
 
 impl Solution {
@@ -34,6 +36,32 @@ impl Solution {
     }
   
     pub fn brute_force(nums: Vec<i32>) -> bool {
+        let mut sum = 0;
+        for num in nums.iter() {
+            sum += num;
+        }
+        
+        if sum % 2 != 0 {
+            return false;
+        }
+        
+        sum /= 2;
+        
+        let counter = u32::pow(2, u32::try_from(nums.len()).unwrap());
+        // Check all combinations
+        for i in 0..counter {
+            let mut tmpSum = 0;
+            for j in 0..nums.len() {
+                if i & (1 << j) != 0 {
+                    tmpSum += nums[j];
+                }
+            }
+            
+            if tmpSum == sum {
+                return true;
+            }
+        }
+        
         false
     }
 }
