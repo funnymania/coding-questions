95d1df2f954ad5653dfc8105a2f89e211751b079
Michael D. McClure <36145527+funnymania@users.noreply.github.com>
2021-06-02 23:02:45 -0700

    Create combination_sum.rs

---
+++ search/combination_sum.rs
@@ -0,0 +1,25 @@
+/// Leetcode 39 https://leetcode.com/problems/combination-sum/
+
+impl Solution {
+    pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        let mut result = Vec::new();
+        let mut currentResult = Vec::new();
+        Solution::find_matches(&candidates, target, 0, &mut result, &mut currentResult, 0);
+        result
+    }
+    
+    fn find_matches(candidates: &Vec<i32>, target: i32, sum: i32, result: &mut Vec<Vec<i32>>, currentResult: &mut Vec<i32>, ptr: usize) {
+        if sum == target {
+            result.push(currentResult.to_vec());
+            return;
+        }
+        if sum > target || ptr == candidates.len() {
+            return;
+        }
+        
+        currentResult.push(candidates[ptr]);
+        Solution::find_matches(candidates, target, sum + candidates[ptr], result, currentResult, ptr);
+        currentResult.pop();
+        Solution::find_matches(candidates, target, sum, result, currentResult, ptr + 1);
+    }
+}
